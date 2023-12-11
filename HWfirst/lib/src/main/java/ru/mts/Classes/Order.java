e4d378effbc3290fd18617f0d6a00738df50bb01
Daniil Shevchenko <danyashev12@mail.ru>
2023-12-12 01:32:40 +0300

    Добавил проверку для скидки

--- HWfirst/lib/src/main/java/ru/mts/Classes/Order.java
+++ HWfirst/lib/src/main/java/ru/mts/Classes/Order.java
@@ -25,12 +25,13 @@ public class Order{
 		if(order == null) {
 			System.out.println("Пустой объект заказа");
 		}
-		else if(order.numOfProducts < 0 || order.sumOfProduct < 0){
+		else if(order.numOfProducts <= 0 || order.sumOfProduct <= 0 || order.sale < 0 || order.sale > 100){
 			System.out.println("Некорректные параметры заказа");
 		}
 		else {
-			System.out.println("Сумма заказа:" + String.format("%.2f", order.numOfProducts*order.sumOfProduct));
-			System.out.println("Сумма заказа со скидкой:" + String.format("%.2f", (order.numOfProducts*order.sumOfProduct)*(100 - order.sale)/100));
+			double sumOfOrder = order.numOfProducts*order.sumOfProduct; //сумма всего заказа без скидки
+			System.out.println("Сумма заказа:" + String.format("%.2f", sumOfOrder));
+			System.out.println("Сумма заказа со скидкой:" + String.format("%.2f", sumOfOrder*(100 - order.sale)/100));
 		}
 	}
 	
