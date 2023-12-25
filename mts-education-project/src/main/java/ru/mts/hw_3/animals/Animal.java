cdb54d75fefb02928b76fb97beae8e1680e309c2
Daniil Shevchenko <danyashev12@mail.ru>
2023-12-25 20:26:32 +0300

    Добавил HW-1 и HW-3

---
+++ mts-education-project/src/main/java/ru/mts/hw_3/animals/Animal.java
@@ -0,0 +1,24 @@
+package ru.mts.hw_3.animals;
+
+import java.math.BigDecimal;
+
+public interface Animal{
+	/**
+	 * getBreed() возвращает породу животного в строковом типе 
+	 */
+	public String getBreed();
+	/**
+	 * getName() возвращает имя животного в строковом типе 
+	 */
+	public String getName();
+	/**
+	 * getCost() возвращает стоимость животного в формате BigDecimal и 
+	 * округленный до двух знаков после запятой
+	 */
+	public BigDecimal getCost();
+	/**
+	 * getCharacter() возвращает характер животного в строковом типе 
+	 */
+	public String getCharacter();
+	
+}
---
+++ mts-education-project/src/main/java/ru/mts/hw_3/animals/Shark.java
@@ -0,0 +1,11 @@
+package ru.mts.hw_3.animals;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Shark extends Predator{
+	
+	public Shark(String breed, String name, BigDecimal cost, String character) {
+		super(breed, name, cost, character);
+	}
+}
