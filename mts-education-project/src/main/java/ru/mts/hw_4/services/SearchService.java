9c387e05aaf893214abe1dca75dfb63d06f2da2f
Daniil Shevchenko <danyashev12@mail.ru>
2023-12-31 00:15:09 +0300

    Убрал модификатор public у методов в интерфейсе

--- mts-education-project/src/main/java/ru/mts/hw_4/services/SearchService.java
+++ mts-education-project/src/main/java/ru/mts/hw_4/services/SearchService.java
@@ -11,18 +11,18 @@ public interface SearchService {
      * @param animals - входной массив жисотных
      * @return Возвращает массив имен животных, которые родились в високосный год
      */
-    public String[] findLeapYearNames(AbstractAnimal[] animals);
+    String[] findLeapYearNames(AbstractAnimal[] animals);
 
     /**
      * @param animals - входной массив животных
      * @param minAge - возраст, старше которого возвращаемые животные
      * @return Возвращает массив животных, возраст которых превышает minAge лет
      */
-    public AbstractAnimal[] findOlderAnimal(AbstractAnimal[] animals, int minAge);
+    AbstractAnimal[] findOlderAnimal(AbstractAnimal[] animals, int minAge);
 
     /**
      * @param animals - входной массив животных
      * Метод выводит на экран дубликаты животных в массиве animals
      */
-    public void findDuplicate(AbstractAnimal[] animals);
+    void findDuplicate(AbstractAnimal[] animals);
 }
